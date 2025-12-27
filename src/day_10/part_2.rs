b5a653417b6f728d4466ed7addc89c72544e4fa6
Jørgen Berntsen <jorgen.berntsen2@vegvesen.no>
2025-12-27 09:36:51 +0100

    all done

--- src/day_10/part_2.rs
+++ src/day_10/part_2.rs
@@ -28,7 +28,16 @@ pub fn solve() {
         .collect::<Vec<Light>>();
     let mut ans = 0;
     for mut l in lines {
-        let a = calc(&mut l.joltage, &l.buttons, &mut HashMap::new());
+        let mut mask = HashMap::new();
+        combinations(
+            0,
+            0,
+            l.joltage.len(),
+            &l.buttons,
+            &mut Vec::new(),
+            &mut mask,
+        );
+        let a = calc(&mut l.joltage, &l.buttons, &mut mask, &mut HashMap::new());
         println!("{a}");
         ans += a;
     }
@@ -36,57 +45,38 @@ pub fn solve() {
     fn calc(
         joltage: &mut Vec<u64>,
         buttons: &Vec<Vec<usize>>,
-        memo: &mut HashMap<i64, Vec<Vec<Vec<usize>>>>,
+        masks: &mut HashMap<i64, Vec<(usize, Vec<usize>)>>,
+        memo: &mut HashMap<Vec<u64>, i64>,
     ) -> i64 {
         if joltage.iter().all(|n| n == &0) {
             return 0;
         }
+        if memo.contains_key(joltage) {
+            return memo[joltage];
+        }
         let target = convert_to_target(joltage);
-
-        if !memo.contains_key(&target) {
-            combinations(
-                target,
-                0,
-                0,
-                joltage.len(),
-                buttons,
-                &mut Vec::new(),
-                memo,
-                true,
-            );
-        }
         let mut m = 1_000_000;
-        if target == 0 {
-            print!("");
+        if !masks.contains_key(&target) {
+            return m;
         }
-        let x = &memo[&target].clone();
-        if target == 0 {
+        let x = &masks[&target].clone();
+
+        'outer: for (presses, butt) in x {
             let mut jolt = joltage.clone();
-            for i in 0..jolt.len() {
-                jolt[i] /= 2;
-            }
-            m = m.min((calc(&mut jolt, buttons, memo) * 2))
-        }
-        'outer: for c in x {
-            let mut jolt = joltage.clone();
-            for k in c {
-                for n in k {
-                    if jolt[*n] == 0 {
-                        continue 'outer;
-                    } else {
-                        jolt[*n] -= 1;
-                    }
+            for n in butt {
+                if (jolt[*n] as i64) - 1 < 0 {
+                    continue 'outer;
+                } else {
+                    jolt[*n] -= 1;
                 }
             }
 
             for i in 0..jolt.len() {
                 jolt[i] /= 2;
             }
-            if m < 70 {
-                print!("");
-            }
-            m = m.min((calc(&mut jolt, buttons, memo) * 2) + c.len() as i64)
+            m = m.min((calc(&mut jolt, buttons, masks, memo) * 2) + *presses as i64)
         }
+        memo.insert(joltage.clone(), m);
         m
     }
 }
@@ -101,54 +91,31 @@ fn convert_to_target(joltage: &Vec<u64>) -> i64 {
 }
 
 fn combinations(
-    target: i64,
     current: i64,
     index: usize,
     length: usize,
     buttons: &Vec<Vec<usize>>,
     combs: &mut Vec<usize>,
-    selected: &mut HashMap<i64, Vec<Vec<Vec<usize>>>>,
-    first: bool,
+    mask: &mut HashMap<i64, Vec<(usize, Vec<usize>)>>,
 ) {
-    if !first && current == target {
-        if combs.len() > 0 {
-            if !selected.contains_key(&target) {
-                selected.insert(target, Vec::new());
-            }
-            let mut a = Vec::new();
-            let x = selected.get_mut(&target).unwrap();
-            for n in combs {
-                a.push(buttons[*n].clone());
-            }
-            x.push(a);
-        }
-        return;
-    }
     if index == buttons.len() {
+        if !mask.contains_key(&current) {
+            mask.insert(current, Vec::new());
+        }
+        mask.get_mut(&current).unwrap().push((
+            combs.len(),
+            combs
+                .iter()
+                .map(|c| buttons[*c].clone())
+                .flatten()
+                .collect(),
+        ));
         return;
     }
     let b = &buttons[index];
     let n = b.iter().fold(0, |acc, n| acc + (1 << ((length - 1) - n)));
     combs.push(index);
-    combinations(
-        target,
-        current ^ n,
-        index + 1,
-        length,
-        buttons,
-        combs,
-        selected,
-        false,
-    );
+    combinations(current ^ n, index + 1, length, buttons, combs, mask);
     combs.pop();
-    combinations(
-        target,
-        current,
-        index + 1,
-        length,
-        buttons,
-        combs,
-        selected,
-        false,
-    );
+    combinations(current, index + 1, length, buttons, combs, mask);
 }
